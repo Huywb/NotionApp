e5e81cd4dd8e2ea308f3a7c9e47041262bbfc69e
huywb <pthhuy1306@gmail.com>
2025-08-24 15:54:09 +0700

    Router and Toaster set up

--- frontend/vite/src/App.tsx
+++ frontend/vite/src/App.tsx
@@ -1,9 +1,18 @@
-import React from 'react'
-
+import { Routes, Route } from 'react-router-dom'
+import HomePage from './pages/HomePage'
+import CreatePage from './pages/CreatePage'
+import NoteDetailPage from './pages/NoteDetailPage'
+import toast from 'react-hot-toast'
 const App = () => {
   return (
     <div>
-        App
+      {toast.success("HEllo")}
+      TTest
+        <Routes>
+          <Route path='/' element={<HomePage></HomePage>}></Route>
+          <Route path='/add' element={<CreatePage></CreatePage>}></Route>
+          <Route path='/notes/:id' element={<NoteDetailPage></NoteDetailPage>}></Route>
+        </Routes>
     </div>
   )
 }
