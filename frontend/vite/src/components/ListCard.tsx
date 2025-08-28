2369ac64ebb90f52a61bbac573cd70bbfcceba55
huywb <pthhuy1306@gmail.com>
2025-08-28 15:59:53 +0700

    ListCard update

--- frontend/vite/src/components/ListCard.tsx
+++ frontend/vite/src/components/ListCard.tsx
@@ -1,36 +1,21 @@
 import React from 'react'
 import Card from './Card'
 
-const ListCard = () => {
+interface ListCardProps {
+    limit: number,
+    number: number
+}
+
+const ListCard: React.FC<ListCardProps> = ({ limit, number }) => {
   return (
     <div className='flex flex-col sm:flex-row  flex-wrap gap-3 p-3 '>
-        <div className='sm:w-[32%]'>
-            <Card></Card>
-        </div>
-        <div className='sm:w-[32%]'>
-            <Card></Card>
-        </div>
-        <div className='sm:w-[32%]'>
-            <Card></Card>
-        </div>
-        <div className='sm:w-[32%]'>
-            <Card></Card>
-        </div>
-        <div className='sm:w-[32%]'>
-            <Card></Card>
-        </div>
-        <div className='sm:w-[32%]'>
-            <Card></Card>
-        </div>
-        <div className='sm:w-[32%]'>
-            <Card></Card>
-        </div>
-        <div className='sm:w-[32%]'>
-            <Card></Card>
-        </div>
-        <div className='sm:w-[32%]'>
-            <Card></Card>
-        </div>
+        {Array.from({length: number}).map((item, index) => (
+            (index<=limit*9 && index>(limit-1)*9)?
+            <div className='sm:w-[32%]' key={index}>
+                <Card id={index}></Card>
+            </div>
+            :null
+        ))}
     </div>
   )
 }
