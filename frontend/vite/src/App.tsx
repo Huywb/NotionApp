d0fec779b05e58e19d30a7075d0c9432aa61b284
huywb <pthhuy1306@gmail.com>
2025-08-29 15:21:43 +0700

    NoteDetail Page Update

--- frontend/vite/src/App.tsx
+++ frontend/vite/src/App.tsx
@@ -2,10 +2,7 @@ import { Routes, Route } from 'react-router-dom'
 import HomePage from './pages/HomePage'
 import CreatePage from './pages/CreatePage'
 import NoteDetailPage from './pages/NoteDetailPage'
-import toast from 'react-hot-toast'
 import Header from './components/Header'
-import ListCard from './components/ListCard'
-import Pagition from './components/Pagition'
 const App = () => {
   return (
     <div className='w-full bg-black text-white min-h-screen '>
--- frontend/vite/src/pages/NoteDetailPage.tsx
+++ frontend/vite/src/pages/NoteDetailPage.tsx
@@ -1,11 +1,16 @@
 import React, { useState } from 'react'
 import { useParams } from 'react-router-dom'
 
+
+
 const NoteDetailPage = () => {
   const params = useParams()
+  console.log(params.id)
+  const title = 'tesst'
+  const content = 'test'
   const [info,setInfo] = useState({
-    title: '',
-    content: ''
+    title: title || '',
+    content: content || ''
   })
   
   const handleSubmit = () => {
@@ -23,19 +28,19 @@ const NoteDetailPage = () => {
   return (
     <div className='px-[10%]  mt-10 h-full flex items-center justify-center'>
         <div className='border-t-green-500 border-t-4 rounded-xl bg-[#212121]  w-[50%] flex flex-col gap-4 items-center py-10'>
-          <h1 className='text-3xl'>Add New Note</h1>
+          <h1 className='text-3xl'>Update Note</h1>
           <div className='flex flex-col w-[80%] text-xl p-4 gap-10 justify-end '>
             <div className='flex  w-full gap-16 items-center   '>
               <label htmlFor="note-title">Title </label>
-              <input name='title' onChange={(e)=>handleChange(e)} className='border rounded-md h-10 w-[80%]' type="text" id="note-title" />
+              <input name='title' value={info.title} onChange={(e)=>handleChange(e)} className='border rounded-md h-10 w-[80%]' type="text" id="note-title" />
             </div>
             <div className='flex w-full gap-8 '>
               <label htmlFor="note-content">Content </label> 
-              <textarea name='content' onChange={(e)=>handleChange(e)} rows={4} className='border rounded-md w-[80%]' id="note-content"></textarea>
+              <textarea name='content' value={info.content} onChange={(e)=>handleChange(e)} rows={4} className='border rounded-md w-[80%]' id="note-content"></textarea>
             </div>
           </div>
 
-          <button onClick={handleSubmit} className='bg-green-500 px-4 py-2 rounded-full text-xl cursor-pointer'>Add Note</button>
+          <button onClick={handleSubmit} className='bg-green-500 px-4 py-2 rounded-full text-xl cursor-pointer'>Update Note</button>
         </div>
     </div>
   )
